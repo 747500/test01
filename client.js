febb4252b1a279af930f30a168af84e68b33486a
Serge R <747500@gmail.com>
2014-03-31 02:36:16 +0400

    simplify

--- client.js
+++ client.js
@@ -7,13 +7,11 @@
 	var JSONStream = require('JSONStream');
 
 	var parser = JSONStream.parse();
+
 	parser.on('root', function (data) {
 		console.log(JSON.stringify(data, null, 2) + '\n');
 	});
 
-	var client = net.connect({ host: '127.0.0.1', port: 25001 });
-	client.on('connect', function () {
-		client.pipe(parser);
-	});
+	net.connect({ host: '127.0.0.1', port: 25001 }).pipe(parser);
 
 })();
