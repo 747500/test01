a07ba328e39c5b7895a0321bc6c875996aa27f3f
Serge R <747500@gmail.com>
2014-03-31 05:10:53 +0400

    added: cluster/fork autoexit

--- client.js
+++ client.js
@@ -7,8 +7,11 @@
 	var JSONStream = require('JSONStream');
 	var config = require('./config.js');
 
-	var parser = JSONStream.parse();
+	process.on('disconnect', function () {
+		process.exit(0);
+	});
 
+	var parser = JSONStream.parse();
 	parser.on('root', function (data) {
 		console.log(JSON.stringify(data, null, 2) + '\n');
 	});
